34c4c0438e7f88d18184ca9df961eed0854cf8c0
Kirill Belovodchenko <belovodchenko2005@mail.ru>
2026-02-25 19:21:55 +0700

    Изменение конфигурации сваггера для более удобного тестирования

--- src/main/java/com/example/hits/application/config/OpenApiConfig.java
+++ src/main/java/com/example/hits/application/config/OpenApiConfig.java
@@ -1,13 +1,28 @@
 package com.example.hits.application.config;
 
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@SecurityScheme(
-        name = "JWT",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        scheme = "bearer"
-)
+@Configuration
 public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        final String securitySchemeName = "bearer-key";
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
+    }
 }
