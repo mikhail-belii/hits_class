857d14b0108a6476a6027f21713563fe71002dfc
Kirill Belovodchenko <belovodchenko2005@mail.ru>
2026-03-08 16:34:59 +0700

    Написание логики утилит комментариев

--- src/main/java/com/example/hits/application/util/PostCommentUtility.java
+++ src/main/java/com/example/hits/application/util/PostCommentUtility.java
@@ -1,5 +1,6 @@
 package com.example.hits.application.util;
 
+import com.example.hits.domain.entity.course.Course;
 import com.example.hits.domain.entity.post.Post;
 import com.example.hits.domain.entity.postcomment.PostComment;
 import com.example.hits.domain.entity.taskanswer.TaskAnswer;
@@ -7,11 +8,13 @@ import com.example.hits.domain.entity.taskanswercomment.TaskAnswerComment;
 import com.example.hits.domain.entity.user.User;
 import lombok.experimental.UtilityClass;
 
+import java.util.Objects;
+
 @UtilityClass
 public class PostCommentUtility {
 
     public boolean isCommentAvailableForEditing(PostComment postComment, User user) {
-        return false;
+        return Objects.equals(user, postComment.getAuthor());
     }
 
 }
